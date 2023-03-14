cb50f284025e0fe6a83668e20652a1725d97ee91
Alan Paul Varghese <alan@geekyants.com>
2023-03-14 11:44:02 +0530

    added inital setup for backend

---
+++ server/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+require("dotenv").config();
+const app=express();
+
+
+const port=process.env.PORT||3000
+
+app.listen(port,()=>{
+    console.log(`server is running at ${port}`);
+})
