0aca67acd73aee98ff4f511945d10542bea7f722
Alan Paul Varghese <alan@geekyants.com>
2023-03-15 11:43:40 +0530

    added app controller

---
+++ client/chatgoclient/lib/controllers/app.dart
@@ -0,0 +1,24 @@
+import 'package:chatgoclient/controllers/base.dart';
+import 'package:chatgoclient/controllers/user_mangement.dart';
+import 'package:chatgoclient/manager/text.dart';
+import 'package:chatgoclient/services/storage/app_storage.dart';
+import 'package:chatgoclient/utils/custom_snack_bar.dart';
+import 'package:flutter/services.dart';
+
+class AppController extends BaseController {
+  AppController._();
+  static AppController instance = AppController._();
+  factory AppController() => instance;
+
+  initApp() async {
+    try {
+      await AppStorage.instance.initAppStorage();
+      await UserMangementController.instance.initToken();
+    } catch (e) {
+      CustomSnackBar.instance
+          .showError(errorText: TextManger.instance.randomError);
+      await Future.delayed(const Duration(seconds: 2));
+      SystemNavigator.pop();
+    }
+  }
+}
--- client/chatgoclient/lib/controllers/user_mangement.dart
+++ client/chatgoclient/lib/controllers/user_mangement.dart
@@ -1,4 +1,5 @@
 import 'package:chatgoclient/data/models/user.dart';
+import 'package:chatgoclient/utils/stroage/user_box.dart';
 import 'package:get/route_manager.dart';
 
 import '../manager/route.dart';
@@ -6,12 +7,27 @@ import '../manager/route.dart';
 class UserMangementController {
   UserMangementController._();
   static UserMangementController instance = UserMangementController._();
+
   factory UserMangementController() => instance;
-  late User currentuser;
-  
+  final UserBoxStorage _userBoxStorage = UserBoxStorage();
+  late User _currentuser;
+  late String? _appToken;
+
+  User get user => _currentuser;
+  String? get token => _appToken;
 
   populateAndNavigateOnAuthentication({required User user}) {
-    currentuser = user;
+    _currentuser = user;
     Get.offAllNamed(Routes.homeScreen);
+  }
+
+  initToken() async {
+    final tokenFromStorage = await _userBoxStorage.getToken() as String?;
+    _appToken = tokenFromStorage;
+  }
+
+  storeToken({required String token}) async {
+    _appToken = token;
+    await _userBoxStorage.storeToken(token: token);
   }
 }
