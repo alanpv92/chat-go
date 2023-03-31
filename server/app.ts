47253d9e8e7b8c9606f4ef4015a1f17915f3511f
Alan Paul Varghese <alan@geekyants.com>
2023-03-31 13:14:48 +0530

    added firebase and notification integration in server

--- server/app.ts
+++ server/app.ts
@@ -11,6 +11,7 @@ const port=process.env.PORT||3000
 
 app.use(express.json());
 
+
 app.use('/webhook',weebhook);
 
 
---
+++ server/services/notification/index.ts
@@ -0,0 +1,38 @@
+import admin from "firebase-admin";
+import { Message } from "firebase-admin/lib/messaging/messaging-api";
+
+const serviceAccountDeatils = require("../../config/firebase-push-notification-key.json");
+
+class FirebaseNotificationService {
+  private static instance: FirebaseNotificationService;
+  private static firebaseApp: admin.app.App;
+  private constructor() {}
+  public static getInstance() {
+    if (!FirebaseNotificationService.instance) {
+      this.instance = new FirebaseNotificationService();
+      this.firebaseApp = admin.initializeApp({
+        credential: admin.credential.cert(serviceAccountDeatils),
+      });
+    }
+    return this.instance;
+  }
+
+  async sendNotification(title: string, body: string, token: string) {
+    try {
+      const message: Message = {
+        notification: {
+          title: title,
+          body: body,
+        },
+        token: token,
+      };
+     let response= await admin.messaging().send(message);
+     
+    } catch (e) {
+        console.log("error is",e)
+    }
+  }
+}
+
+
+export default FirebaseNotificationService;
