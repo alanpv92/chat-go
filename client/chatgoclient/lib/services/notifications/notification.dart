5749099c30696d5a3c78946a69bd4496f21d34f0
Alan Paul Varghese <alan@geekyants.com>
2023-03-31 13:26:40 +0530

    added some changes

---
+++ client/chatgoclient/lib/services/notifications/notification.dart
@@ -0,0 +1,85 @@
+import 'dart:math';
+import 'dart:developer' as dev;
+import 'package:app_settings/app_settings.dart';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+@pragma('vm:entry-point')
+Future firebaseMessagingBackgroundHandler(RemoteMessage message) async {}
+
+class NotificationService {
+  NotificationService._();
+  static NotificationService instance = NotificationService._();
+  factory NotificationService() => instance;
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  initLocalNotifications(RemoteMessage message) async {
+    var andriodInitializationSettings =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iosIntializationSettings = const DarwinInitializationSettings();
+    var initializationSetting = InitializationSettings(
+        android: andriodInitializationSettings, iOS: iosIntializationSettings);
+    await _flutterLocalNotificationsPlugin.initialize(
+      initializationSetting,
+      onDidReceiveNotificationResponse: (details) {},
+    );
+  }
+
+  Future<void> showNotification(RemoteMessage message) async {
+    AndroidNotificationChannel androidNotificationChannel =
+        AndroidNotificationChannel(Random.secure().nextInt(100000).toString(),
+            'High Importance Notifications',
+            importance: Importance.max);
+    AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(androidNotificationChannel.id.toString(),
+            androidNotificationChannel.name.toString(),
+            channelDescription: '',
+            importance: Importance.high,
+            priority: Priority.high,
+            ticker: 'ticker');
+    DarwinNotificationDetails darwinNotificationDetails =
+        const DarwinNotificationDetails();
+    NotificationDetails notificationDetails = NotificationDetails(
+        android: androidNotificationDetails, iOS: darwinNotificationDetails);
+    _flutterLocalNotificationsPlugin.show(0, message.notification!.title,
+        message.notification!.body, notificationDetails);
+  }
+
+  initFirebaseNotifications() {
+    FirebaseMessaging.onMessage.listen((event) async {
+      await initLocalNotifications(event);
+      showNotification(event);
+    });
+  }
+
+  requestNotificationPermission() async {
+    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      announcement: true,
+      badge: true,
+      carPlay: true,
+      criticalAlert: true,
+      provisional: true,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      getDeviceToken();
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+    } else {
+      await AppSettings.openNotificationSettings();
+    }
+  }
+
+  Future<String?> getDeviceToken() async {
+    String? token = await _firebaseMessaging.getToken();
+    dev.log(token.toString());
+    return token;
+  }
+}
